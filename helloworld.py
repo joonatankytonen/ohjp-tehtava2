0f179acb7d00dad25a4b25ffc05112b81c4d4a38
juhay-git <juha.yli-hemminki@seamk.fi>
2024-09-27 09:53:20 +0300

    Lisätty tulostuksia

--- helloworld.py
+++ helloworld.py
@@ -2,3 +2,14 @@ message = "Hello World!"
 
 for i in range(10):
 	print(message)
+
+#tässä lisää tervehdyksiä
+def hello(n):
+    if n == 1:
+        print("ja moi!")
+        return 1
+    else:
+        print(message)
+        return hello(n - 1)
+    
+hello(10)
